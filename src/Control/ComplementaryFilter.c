8a8193fd462f4af4766a8d8d65349698afde672c
Abiel <empoleom@gmail.com>
2020-08-18 01:21:22 -0500

    Added complementary filter

--- src/Control/ComplementaryFilter.c
+++ src/Control/ComplementaryFilter.c
@@ -5,3 +5,12 @@
 #include <stdint.h>
 #include "ComplementaryFilter.h"
 
+void estimatorComplementary(State * state, SensorData *data, const uint32_t tick){
+    if(RATE_DO_EXECUTE(ATTITUDE_UPDATE_RATE, tick)){
+        updateVelocityEstimate(state->acc.z, ATTITUDE_UPDATE_DT);
+    }
+
+    if(RATE_DO_EXECUTE(POS_UPDATE_RATE, tick)){
+        updatePositionEstimate(state, data, POS_UPDATE_DT);
+    }
+}
--- src/Control/ComplementaryFilter.h
+++ src/Control/ComplementaryFilter.h
@@ -6,10 +6,19 @@
 #define MICRODRONEFIRMWARE_COMPLEMENTARYFILTER_H
 
 #include "DataTypes/ControlTypes.h"
+#include "FreeRTOS.h"
 
-#define RATE_DO_EXECUTE(RATE_HZ, TICK) ((TICK % (RATE_MAIN_LOOP / RATE_HZ)) == 0)
+#define RATE_DO_EXECUTE(RATE_HZ, TICK) ((TICK % (configTICK_RATE_HZ / RATE_HZ)) == 0)
+
+#define ATTITUDE_UPDATE_RATE 250
+#define ATTITUDE_UPDATE_DT 1.0/ATTITUDE_UPDATE_RATE
+
+#define POS_UPDATE_RATE 100
+#define POS_UPDATE_DT 1.0/POS_UPDATE_RATE
+
+void estimatorComplementary(State * state, SensorData *data, uint32_t tick);
 
 void updatePositionEstimate(State * estimate, SensorData const * sensorData, float dt);
-void updateVelocityEstimate();
+void updateVelocityEstimate(float accWZ, float dt);
 
 #endif //MICRODRONEFIRMWARE_COMPLEMENTARYFILTER_H
