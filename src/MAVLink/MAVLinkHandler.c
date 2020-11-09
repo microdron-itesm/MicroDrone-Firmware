8c8439327d7d1bceb981cba3abf76f9ed8810027
Abiel <empoleom@gmail.com>
2020-11-09 12:13:12 -0600

    Added exit from e-stop

--- src/MAVLink/MAVLinkHandler.c
+++ src/MAVLink/MAVLinkHandler.c
@@ -32,6 +32,9 @@ void handle_MAVLink_message(mavlink_message_t *msg){
             if(hb.system_status == MAV_STATE_EMERGENCY){
                 //Panik
                 hal_motors_enable(false);
+            } else {
+                //Exit from panik
+                hal_motors_enable(true);
             }
             break;
     }
