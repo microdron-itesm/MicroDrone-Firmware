da9ed0378f096faba9730fc1fd4a96887a8c7f12
Abiel <empoleom@gmail.com>
2020-11-09 12:00:46 -0600

    Fixed PC comms

--- HAL/PC/comms.c
+++ HAL/PC/comms.c
@@ -8,6 +8,7 @@
 #include <UDP.h>
 #include "PC_HAL_CONFIG.h"
 
+static uint8_t buffer[1024];
 static udp_conn_data comms_connData;
 
 ssize_t hal_comms_init(void *options, size_t size){
@@ -21,8 +22,15 @@ ssize_t hal_comms_send_buffer(uint8_t *buf, size_t len){
     return udp_conn_send(&comms_connData, buf, len);
 }
 
-ssize_t hal_comms_recv_buffer(uint8_t *buf, size_t buf_len){
-    return udp_conn_recv(&comms_connData, buf, buf_len);
+ssize_t hal_comms_recv_buffer(mavlink_message_t *msg, mavlink_status_t* status){
+    ssize_t recvSize = udp_conn_recv(&comms_connData, buffer, sizeof(buffer));
+    for(ssize_t i = 0; i < recvSize; i++){
+        if(mavlink_parse_char(MAVLINK_COMM_0, buffer[i], msg, status)){
+            return 1;
+        }
+    }
+
+    return -1;
 }
 
 ssize_t hal_comms_close(){
